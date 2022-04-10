79b02a9bf759d98b13635298e5a29b41b498a109
Mraihi Amine <mraihiamin@gmail.com>
2022-04-10 13:12:00 +0100

    Adding qualifier to resolve the issue of multiple beans with the same type

---
+++ src/main/java/com/qualifier/qualifier/Service/Bike.java
@@ -0,0 +1,19 @@
+package com.qualifier.qualifier.Service;
+
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("bikeBean")
+public class Bike implements Vehicle{
+    @Override
+    public void start() {
+        System.out.println("Bike started");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Bike stopped");
+    }
+}
---
+++ src/main/java/com/qualifier/qualifier/Service/Car.java
@@ -0,0 +1,18 @@
+package com.qualifier.qualifier.Service;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("carBean")
+public class Car implements Vehicle{
+    @Override
+    public void start() {
+        System.out.println("Car started");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Car stopped");
+    }
+}
